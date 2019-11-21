326f8b95906d2b03a178ad0512e9ad9a11da700c
jwymanumich <jwyman@umich.edu>
2019-11-20 21:30:10 -0500

    changes for wordcloud

--- twitter-summarizer/src/app/search/search.component.ts
+++ twitter-summarizer/src/app/search/search.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { TwitterTopicService } from '../twitter_topic/twitter_topic.service';
+    
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  providers: [TwitterTopicService]
 })
+
 export class SearchComponent implements OnInit {
 
-  title = 'Twitzer';
+  title: string = 'Twitzer';
 
   placeholder: string = '@Matt_LeBlanc';
 
   handle: string;
 
-  constructor(private router: Router) { }
+  mystring: string;
+
+  constructor(private router: Router, private twittertopicservice: TwitterTopicService) { }
 
   ngOnInit() {
   }
@@ -29,8 +34,22 @@ export class SearchComponent implements OnInit {
     }
   }
 
-  search() {
+  async search() {
     console.log(`/summary/${this.handle}`);
+//    var a = this.twittertopicservice.getTwitterTopics()
+    
+//    var a = this.twittertopicservice.getTwitterTopics().subscribe((x: string) => {
+//                  this.title = x;
+//                  this.placeholder = x;
+//                  this.handle = x;
+//    })
+
+//    .subscribe((e: KeyboardEvent) => {
+//        if (e.keyCode === ESC_KEY) {
+//          nameInput.value = '';
+//        }
+
+    console.log('afff'); 
     this.router.navigateByUrl(`/summary/${this.handle}`);
   }
 
